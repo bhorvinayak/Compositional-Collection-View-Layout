63f38566bd4c39b60a65bc88c0d5d7ef84f7997f
Vishal <vishal.n@intelegain.com>
2020-05-25 14:22:09 +0530

    Initial Commit

---
+++ OrthogonalCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrthogonalCollectionView
+//
+//  Created by Vinayak Bhor on 25/05/20.
+//  Copyright © 2020 Vinayak Bhor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
